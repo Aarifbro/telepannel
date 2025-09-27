0080b3548816b9d38bf42d394b618cd2fcf2e8c1
kinetic <adorableboi5@gmail.com>
2025-09-27 19:47:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,12 @@ ADMIN_ID = 1513264586 # Enter your numeric user ID
 CRYPTO_ADDRESS = "bc1qe5l2pzz3nytre346e04tjnjsa9ztz5tzz9gzfy" 
 
 # --- Force Join Configuration ---
-FORCE_CHANNEL_ID = -1002422874306 # Enter your channel's numeric ID
-FORCE_CHANNEL_ID = -1003176211335 # Enter your channel's invite link
-FORCE_CHANNEL_ID = -1002148162931 # Enter your channel's invite link
+# To force users to join multiple channels, put all the IDs in a list.
+FORCE_CHANNEL_IDS = [
+    -1002422874306,
+    -1003176211335,
+    -1002148162931
+]
 
 # --- File & Database Configuration ---
 DB_NAME = "shop_bot.db"
